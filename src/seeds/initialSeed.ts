cc92bd2669c123d4d9daba6421a820b9a328d22f
0xVance <110498499+0xVance@users.noreply.github.com>
2022-12-10 04:06:10 +0000

    update initial seed

--- src/seeds/initialSeed.ts
+++ src/seeds/initialSeed.ts
@@ -32,7 +32,7 @@ const seedRewards = async () => {
   const rewards = [
     {
       description:
-        'A journey through space-time. A traversal of the lavendar sea. Discoveries and dangers beyond the imagination. \n\nStop. Think. Breathe. This expedition is the start of a journey larger than life. Are you ready? \n\nThis NFT was claimable for 50 Fragments in the initial Swapr Expeditions event.',
+        'A journey through space-time. A traversal of the lavendar sea. Discoveries and dangers beyond the imagination. \n\nStop. Think. Breathe. This expedition is the start of a journey larger than life. Are you ready?',
       imageURI: `ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/1.png`,
       name: `Introspection`,
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -43,7 +43,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'You dream of a world plagued. The Pestilence paints a beautiful mockery of life and death. So... familiar. \nThis NFT was claimable for 100 Fragments in the initial Swapr Expeditions event. \nIt grants the “scourge” profile accent at https://swapr.eth.limo/ to the NFT holder.',
+        'You dream of a world plagued. The Pestilence paints a beautiful mockery of life and death. So... familiar. \n\nIt grants the “scourge” profile accent at https://swapr.eth.limo/ to the NFT holder.',
       imageURI: `ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/2.png`,
       name: `Visions of Scourge`,
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -54,7 +54,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'A shell of its former self, your home planet lay in a state of disarray. Mysterious creatures plague the surface, eradicating what little life remains in the once prosperous world. You WILL find a better world for your race. This NFT was claimable for 200 Fragments in the initial Swapr Expeditions event.',
+        'A shell of its former self, your home planet lay in a state of disarray. Mysterious creatures plague the surface, eradicating what little life remains in the once prosperous world. \n\nYou WILL find a better world for your race.',
       imageURI: 'ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/3.png',
       name: 'Birth',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -65,7 +65,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'A shell of its former self, your home planet lay in a state of disarray. Mysterious creatures plague the surface, eradicating what little life remains in the once prosperous world. You WILL find a better world for your race. This NFT was claimable for 200 Fragments in the initial Swapr Expeditions event.',
+        'Elaborate visions of a perfect world. Your home now a distant memory in the far reaches of your mind. \n\nIt grants the “creation” profile accent at https://swapr.eth.limo/ to the NFT holder.',
       imageURI: 'ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/4.png',
       name: 'Visions of Creation',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -76,7 +76,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'Have you made peace with your future? A life of pilgrimage and uncertainty. Prospect and vulnerability. You gather yourself and return to your vessel. This NFT was claimable for 450 Fragments in the initial Swapr Expeditions event.',
+        'Have you made peace with your future? A life of pilgrimage and uncertainty. Prospect and vulnerability. \n\nYou gather yourself and return to your vessel.',
       imageURI: 'ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/5.png',
       name: 'Wayfarer',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -87,7 +87,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'You dream of draught. THe fascinating environment that was stripped away from your world. Beauty beyond the beholder. This NFT was claimable for 600 Fragments in the initial Swapr Expeditions event. It grants the “depth” profile accent at https://swapr.eth.limo/ to the NFT holder.',
+        'You dream of draught. THe fascinating environment that was stripped away from your world. Beauty beyond the beholder. \n\nIt grants the “depth” profile accent at https://swapr.eth.limo/ to the NFT holder.',
       imageURI: 'ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/6.png',
       name: 'Visions of Depth',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -98,7 +98,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'A life of yearning. Searching for somewhere that may not exist. You make peace with your arrangement and take one final look at your home. This NFT was claimable for 800 Fragments in the initial Swapr Expeditions event.',
+        'A life of yearning. Searching for somewhere that may not exist. \n\nYou make peace with your arrangement and take one final look at your home.',
       imageURI: 'ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/7.png',
       name: 'Preparations',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -109,7 +109,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'You dream of the end. destruction and death beyond comprehension. A universe that lay in ruin. This NFT was claimable for 1000 Fragments in the initial Swapr Expeditions event. It grants the “cessation” profile accent at https://swapr.eth.limo/ to the NFT holder.',
+        'You dream of the end. destruction and death beyond comprehension. A universe that lay in ruin. \n\nIt grants the “cessation” profile accent at https://swapr.eth.limo/ to the NFT holder.',
       imageURI: 'ipfs:/QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/8.png',
       name: 'Visions of Cessation',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -120,7 +120,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'Adventure lay ahead. The fate of you and your people rests solely in your hands. You fire the engines of your small craft and, in a fiery blaze, exit the atmosphere. This NFT was claimable for 1250 Fragments in the initial Swapr Expeditions event.',
+        'Adventure lay ahead. The fate of you and your people rests solely in your hands. \n\nYou fire the engines of your small craft and, in a fiery blaze, exit the atmosphere.',
       imageURI: 'ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/9.png',
       name: 'Launch',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
@@ -131,7 +131,7 @@ const seedRewards = async () => {
     },
     {
       description:
-        'You dream of splendor. A vision so surreal, representing the resounding success of your journey. This NFT was claimable for 1500 Fragments in the initial Swapr Expeditions event. It grants the “grandeur” profile accent at https://swapr.eth.limo/ to the NFT holder.',
+        'You dream of splendor. A vision so surreal, representing the resounding success of your journey. \n\nIt grants the “grandeur” profile accent at https://swapr.eth.limo/ to the NFT holder.',
       imageURI: 'ipfs://QmRo7LhZFJHkVqKkQL9ySbEX1pyPXVvUZ7Xz9zDQNK765f/10.png',
       name: 'Visions of Grandeur',
       nftAddress: process.env.NFT_CONTRACT_ADDRESS || '',
